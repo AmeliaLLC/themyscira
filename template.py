cffef7f17b9539e27bea4fa3c1d0c5dd5747055d
Paul Hummer <paul@eventuallyanyway.com>
2013-01-06 16:26:54 -0700

    Extract the navigation header out into its own template

--- template.py
+++ template.py
@@ -2,21 +2,24 @@ import pybars
 
 
 HELPERS = {}
+PARTIALS = {
+    'navigation': pybars.Compiler().compile(open('templates/navigation.handlebars').read().decode('UTF-8'))
+}
 
 def render(source, context, use_base=True):
     compiler = pybars.Compiler()
 
     with open(source) as f:
-        template_contents = unicode(f.read())
+        template_contents = unicode(f.read().decode('utf-8'))
     template = compiler.compile(template_contents)
 
     if use_base:
         with open('templates/base.handlebars') as f:
             main_contents = f.read().decode('utf-8')
         main_template = compiler.compile(main_contents)
-        output = main_template(context, helpers=HELPERS, partials={
-            'content': template
-        })
+        partials = dict(PARTIALS, content=template)
+        output = main_template(
+            context, helpers=HELPERS, partials=partials)
     else:
         output = template(context, helpers=HELPERS)
 
